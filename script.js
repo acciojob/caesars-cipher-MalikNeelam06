d55a97d41a34cb3648ff8098ce0373585e35a859
MalikNeelam06 <128276286+MalikNeelam06@users.noreply.github.com>
2023-07-23 15:56:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -33,10 +33,11 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
+  let decodedArr = ''; // Your Result goes here
   // Only change code below this line
 	for (let i = 0; i<encodedStr.length; i++) {
-		decodedArr.push(lookup[encodedStr[i]]);
+		// decodedArr.push(lookup[encodedStr[i]]);
+		decodedArr+=lookup[encodedStr[i]]
 	}
 	console.log(decodedArr);
 // decodedArr.push(lookup.encodedStr);
